107d54605153a94e5963fb18c42afdf5bfa933aa
Teja6423 <tejachevuru65@gmail.com>
2025-02-22 08:29:58 +0530

    Responsive updates
    Added some new featured for mobile devices. More responsive and optimized data sending and receiving

--- app.js
+++ app.js
@@ -24,9 +24,7 @@ const db = new Pool({
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: false
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
